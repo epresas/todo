3e137c253160d55dfb6de91cedc8ab311e53a2cd
Edmundo Presas <ednmj1@gmail.com>
2017-02-20 18:26:28 +0100

    cleaning up code

--- js/mainScript.js
+++ js/mainScript.js
@@ -2,48 +2,27 @@
 $('#todoTextInput').focus();
 var todoList = {
 	todos: [],
-  	displayTodo: function(){
-    if(this.todos.length === 0){
-      console.log('The list is empty');
-    } 
-    else {
-      console.log('My Todos');
-      for(var i = 0; i < this.todos.length;i++){
-      
-        if(this.todos[i].completed === true){
-          console.log('(x)',this.todos[i].todoText);
-        }
-        else {
-          console.log('( )',this.todos[i].todoText);
-        }
-      }
-    }
-  },
-  
+
   addTodo: function(todoText){
     this.todos.push({
       todoText: todoText,
       completed: false
     });
-    this.displayTodo();
     $('#todoTextInput').focus();
   },
   
   changeTodo: function(position,todoText){
     this.todos[position].todoText = todoText;
-    this.displayTodo();
     $('#todoTextInput').focus();
   },
   
   deleteTodo: function (position){
     this.todos.splice(position,1);
-    this.displayTodo();
     $('#todoTextInput').focus();
   },
   toggleCompleted: function(position){
     var todo = this.todos[position];
     todo.completed = !todo.completed;
-    this.displayTodo();
   },
   toggleAll: function(){
     var totalTodos = this.todos.length;
@@ -64,22 +43,20 @@ var todoList = {
       }
        
     }
-    this.displayTodo();
   }
 
 };
 
 var handlers={
-  displayTodo: function(){
-    todoList.displayTodo();
-  },
   addTodo: function(){
     var todoInput= $('#todoTextInput').val();
     todoList.addTodo(todoInput);
     $('#todoTextInput').val("");
+    view.displayTodo();
   },
   toggleAll:function(){
     todoList.toggleAll();
+   	view.displayTodo();
   }
 };
 
@@ -98,15 +75,11 @@ var view = {
 
 $("#todoTextInput").keypress(function( event ) {
 	if ( event.which == 13 ) {
-	     handlers.addTodo();
-	     view.displayTodo();
+	     handlers.addTodo();  
 	}
 });
 
-$('#addTodoBtn').click(function(){
-	handlers.addTodo();
-	view.displayTodo();
-});
+
 $('.btnRemoveTodo').click( function(){
 	var li = $(this).index();
 	
