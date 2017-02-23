300d6618ea3d49b52191cb63ae2445a478302f67
Edmundo Presas <ednmj1@gmail.com>
2017-02-23 17:36:43 +0100

    add edit btn

--- js/mainScript.js
+++ js/mainScript.js
@@ -48,7 +48,20 @@ var todoList = {
 };
 
 var handlers={
-  addTodo: function(){
+  addTodo: function(event){
+    var todoInput= $('#todoTextInput').val();
+    if (event.which == 13) {
+      
+      if (todoInput === "") {
+        alert('Text field cannot be empty!');
+      } else {
+      todoList.addTodo(todoInput);
+      $('#todoTextInput').val("");
+      view.displayTodo();
+      }
+    }
+  },
+  addTodoClick: function(){
     var todoInput= $('#todoTextInput').val();
     if (todoInput === "") {
       alert('Text field cannot be empty!');
@@ -58,8 +71,15 @@ var handlers={
     view.displayTodo();
     }
   },
+  changeTodo: function(position,todoText){
+
+  },
   deleteTodo: function(position){
     todoList.deleteTodo(position);
+    view.displayTodo();
+  },
+  toggleCompleted: function(position){
+    todoList.toggleCompleted(position);
     view.displayTodo();
   },
   toggleAll:function(){
@@ -81,6 +101,7 @@ var view = {
         $(todoLi).html('<span class="glyphicon glyphicon-check"></span><span class="todoLiText">'+ todo.todoText +'</span>');
       } 
       $(todoLi).append(this.addDeleteBtn());
+      $(todoLi).append(this.addEditBtn());
       $(todoUl).append(todoLi);
     }, this); //this hace referencia al this del objeto, y se añade para que la instruccion this dentro de la funcion callback llame al objeto tambien.
     
@@ -89,28 +110,54 @@ var view = {
     var deleteBtn = document.createElement('button');
     $(deleteBtn).addClass("deleteBtn").html('<span class="glyphicon glyphicon-trash"></span>');
     return deleteBtn;
+  },
+  addEditBtn: function(){
+    var editBtn = document.createElement('button');
+    $(editBtn).addClass("editBtn").html('<span class="glyphicon glyphicon-edit"></span>');
+    return editBtn;  
   }
 };
 
-//################# OBJECTS ##################################################
+//################# END OBJECTS ##################################################
 
   var todoUl = document.querySelector('ul');
   todoUl.addEventListener("click", function(event){
     var clickedElement = event.target;
-    if(clickedElement.className === 'deleteBtn'){
-    handlers.deleteTodo(parseInt(clickedElement.parentNode.id));
+
+    switch (clickedElement.className){
+      case "deleteBtn":
+        handlers.deleteTodo(parseInt(clickedElement.parentNode.id));
+        break;
+      case "editBtn":
+        handlers.changeTodo(parseInt(clickedElement.parentNode.id),/*funcion que edite el texto*/);
+        break;
+      case "glyphicon glyphicon-trash":
+        handlers.deleteTodo(parseInt(clickedElement.parentNode.id));
+        break;
+      case "glyphicon glyphicon-unchecked":
+        handlers.toggleCompleted(parseInt(clickedElement.parentNode.id));
+        var classText = $('.todoLiText');
+        classText.addClass('todoTextCompleted');
+        
+        break;
+      case "glyphicon glyphicon-check":
+        handlers.toggleCompleted(parseInt(clickedElement.parentNode.id));
+        var text= document.getElementById('todoLiText');
+        text.className -= 'todoTextCompleted';
+
+        break;
     }
   });
 
-  $("#todoTextInput").keypress(function(event) {
-  	if (event.which == 13) {
-      if($("#todoTextInput").val() === ""){
-        alert('Text field cannot be empty!');  
-      } else {
-  	     handlers.addTodo();  
-        } 
-  	}
-  });
+  // $("#todoTextInput").keypress(function(event) {
+  // 	if (event.which == 13) {
+  //     if($("#todoTextInput").val() === ""){
+  //       alert('Text field cannot be empty!');  
+  //     } else {
+  // 	     handlers.addTodo();  
+  //       } 
+  // 	}
+  // });
 
 
 
